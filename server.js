90d07f12521b4a4ea4bf489924c09e4463cf9c57
shiitakeo <box.roykutya@gmail.com>
2013-03-04 14:28:14 +0900

    とりあえず個別のビデオチャットができる版

--- server.js
+++ server.js
@@ -12,10 +12,29 @@ var io = require('socket.io')
 ,   io = io.listen(server)
 ;
 
-io.sockets.on('connection', function(socket) {
+videoChat = io.sockets.on('connection', function(socket) {
   console.log("connect : " + socket);
+  
   socket.on('message', function(data){
+    var room;
     console.log("recieve : " + data);  
-    socket.broadcast.emit('message', data);
+    //socket.broadcast.emit('message', data);
+    socket.get('room', function(err, _room){
+      room = _room;  
+    });
+    videoChat.to(room).emit('message', data);
+  });
+  //socket.on('disconnect', function(){
+   // console.log("discon : " + socket);  
+   // socket.broadcast.emit("disconnect", socket);
+ // });
+  socket.on('roomId', function(data){
+    console.log("roomId : " + data);  
+    socket.set('room', data);
+    videoChat.to(data).emit('rooomId', socket + 'さんが入室');
+    socket.join(data);
+    //socket.broadcast.emit('roomId', data);
+    videoChat.to(data).emit('roomId', socket + 'がきた');
   });
 });
+
